3cec244515cfb74a5c03e17ca815cd178fde71ab
sydorenkovd <sydorenkovd@gmail.com>
2017-06-28 22:17:03 +0300

    login/logout functionality

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -36,4 +36,11 @@ class SecurityController extends Controller
         ));
     }
 
+    /**
+     * @Route("/logout", name="security_logout")
+     */
+    public function logoutAction() {
+        throw new \Exception('this should not be reached');
+    }
+
 }
