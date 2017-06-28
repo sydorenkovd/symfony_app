6b0b718a28eb9ae397c10aa53d666049475f70ad
sydorenkovd <sydorenkovd@gmail.com>
2017-06-28 22:47:46 +0300

    delete useless code from test

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -14,11 +14,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class MainController extends Controller
 {
     public function homepageAction() {
-        $em = $this->getDoctrine()->getManager();
-        $user = $em->getRepository('AppBundle:User')->find(1);
-        $user->setPassword('ilike');
-        $em->persist($user);
-        $em->flush();
         return $this->render('main/homepage.html.twig');
     }
 }
